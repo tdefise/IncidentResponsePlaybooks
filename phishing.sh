264f0d36ce4f931bf274c7c9749c123f030e3772
Thomas Defise <t.defise@gmail.com>
2020-11-06 20:09:09 +0100

    Changed the wordlist

--- phishing.sh
+++ phishing.sh
@@ -6,4 +6,4 @@ $UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 $URL=""
 
 echo "Starting the enumeration of $URL using gobuster"
-gobuster dir -u $URL -w directory-list-2.3-small.txt -o gobuster_raft.txt
+gobuster dir -u $URL -w raft-small-directories.txt -o gobuster_raft.txt
